d87d93ccd45de1c96bb402004ff118a417dfcc19
subsash Timalsina <subasj@gmail.com>
2026-09-23 12:38:11 +0545

    Initial commit

---
+++ tests/APIpost.spec.js
@@ -0,0 +1,9 @@
+import { expect, test } from '@playwright/test';
+test ('get through api',async({ request})=>{
+    const response= await request.get('https://jsonplaceholder.typicode.com/posts/1');
+    expect (response.status()).toBe(200);
+
+    const data= await response.json();
+    console.log("user data:",data);
+    expect(data.id).toBe(1);
+})
